b9e404b6ff1aaa4e4d459b5f8799a36e64e362fd
wiliam <wiliam.trioullier@gmail.com>
2022-12-23 16:23:41 +0100

    Deuxième commit.
    Exercice pas complèter.

---
+++ programme.js
@@ -0,0 +1,14 @@
+const quantiteDeNom = prompt("Combien de nom voulez-vous ?"); 
+const name = [];
+
+
+    for (let i = 0; 1 < parseInt(quantiteDeNom); i++){
+        name.push(prompt("Saisissez un nom."))
+    if
+}
+
+console.log(quantiteDeNom)
+console.log(name)
+
+
+
